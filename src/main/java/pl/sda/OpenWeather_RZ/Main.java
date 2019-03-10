f58f33944d18031782367dfa8daa9944e349b6ac
Remigiusz Zudzin <remik.zudzin@gmail.com>
2019-03-10 14:05:35 +0100

    dzialajacy program

--- src/main/java/pl/sda/OpenWeather_RZ/Main.java
+++ src/main/java/pl/sda/OpenWeather_RZ/Main.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * @author Remigiusz Zudzin
@@ -14,16 +15,15 @@ public class Main {
         ObjectMapper mapper = new ObjectMapper();
 
         try {
-            Weather weather = mapper.readValue(new File
-                    ("http://api.apixu.com/v1/current.json?key=75ef5138a56b445faf3120447191003&q=" +
-                            location), Weather.class);
+            Weather weather = mapper.readValue(new URL("http://api.apixu.com/v1/current.json?key=" +
+                    "75ef5138a56b445faf3120447191003&q=" + location), Weather.class);
             System.out.println("Nazwa miejscowości: " + weather.getLocation().getName());
             System.out.println("Czas: " + weather.getLocation().getLocaltime());
             System.out.println("Wspolrzedne miejscowości: ");
-            System.out.println("Długość geograficzna: " + weather.getLocation().getLat());
-            System.out.println("Szerokość geograficzna: " + weather.getLocation().getLon());
-            System.out.println("Temperatura: " + weather.getCurrent().getTemp_c());
-            System.out.println("Zachmurzenie: " + weather.getCurrent().getCondition().getText());
+            System.out.println("Długość geograficzna: " + weather.getLocation().getLat() + " stopni");
+            System.out.println("Szerokość geograficzna: " + weather.getLocation().getLon() + " stopni");
+            System.out.println("Temperatura: " + weather.getCurrent().getTemp_c() + " stopni Celsjusza");
+            System.out.println("Pogoda ogólnie: " + weather.getCurrent().getCondition().getText());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -32,7 +32,18 @@ public class Main {
     public static void main(String[] args) {
 
         readJsonWeatherDataSet("Bydgoszcz");
-
+        System.out.println();
+        readJsonWeatherDataSet("Lobzenica");
+        System.out.println();
+        readJsonWeatherDataSet("Paris");
+        System.out.println();
+        readJsonWeatherDataSet("Warszawa");
+        System.out.println();
+        readJsonWeatherDataSet("Lodz");
+        System.out.println();
+        readJsonWeatherDataSet("Gdansk");
+        System.out.println();
+        readJsonWeatherDataSet("New_York");
     }
 
 }
